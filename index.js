43b24641e6392b249d3bed707f187d405b3c4fec
Hellomon23 <106308874+Hellomon23@users.noreply.github.com>
2022-07-25 20:23:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const dashboard = http.createServer(function (_req, res) {
     res.end();
 });
 
-dashboard.listen(process.env.port || 0);
+dashboard.listen(8080);
 
 logger("Opened server site...", "[ Starting ]");
 
@@ -42,12 +42,14 @@ function startBot(message) {
         shell: true
     });
 
-    child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Restarting...");
-            global.countRestart += 1;
-            return;
-        } else return;
+    child.on("close",async (codeExit) => {
+      var x = 'codeExit'.replace('codeExit',codeExit);
+        if (codeExit == 1) return startBot("Restarting...");
+         else if (x.indexOf(2) == 0) {
+           await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000));
+                 startBot("Open ...");
+       }
+         else return; 
     });
 
     child.on("error", function (error) {
@@ -59,5 +61,51 @@ function startBot(message) {
 ////////////////////////////////////////////////
 
 
-function _0x3e84(){var _0x40e4a4=['28oSZEJl','Version:\x20','[\x20VERSION\x20]','10iXguqx','data','928836kjiAIn','[\x20NAME\x20]','20HogeXy','https://raw.githubusercontent.com/d-jukie/miraiv2/main/package.json','64026aHYjjv','1162444WZEGSh','338696YhJJXw','description','name','475419mNCHVY','version','2wspsLs','447072MUODkE','then','1404469inXkSU'];_0x3e84=function(){return _0x40e4a4;};return _0x3e84();}var _0x5d5628=_0x94a2;function _0x94a2(_0x1f7fc1,_0x3b3d6e){var _0x3e84ec=_0x3e84();return _0x94a2=function(_0x94a209,_0x54de12){_0x94a209=_0x94a209-0x6e;var _0x2671df=_0x3e84ec[_0x94a209];return _0x2671df;},_0x94a2(_0x1f7fc1,_0x3b3d6e);}(function(_0x3620bb,_0x543a5f){var _0x23d642=_0x94a2,_0x3d7b49=_0x3620bb();while(!![]){try{var _0x4c824b=-parseInt(_0x23d642(0x81))/0x1+-parseInt(_0x23d642(0x74))/0x2*(parseInt(_0x23d642(0x72))/0x3)+parseInt(_0x23d642(0x6e))/0x4+parseInt(_0x23d642(0x7f))/0x5*(parseInt(_0x23d642(0x75))/0x6)+parseInt(_0x23d642(0x78))/0x7*(-parseInt(_0x23d642(0x6f))/0x8)+parseInt(_0x23d642(0x7d))/0x9+-parseInt(_0x23d642(0x7b))/0xa*(parseInt(_0x23d642(0x77))/0xb);if(_0x4c824b===_0x543a5f)break;else _0x3d7b49['push'](_0x3d7b49['shift']());}catch(_0x1e8d7a){_0x3d7b49['push'](_0x3d7b49['shift']());}}}(_0x3e84,0x2a131),axios['get'](_0x5d5628(0x80))[_0x5d5628(0x76)](_0x17a17b=>{var _0xd4f7f9=_0x5d5628;logger(_0x17a17b[_0xd4f7f9(0x7c)][_0xd4f7f9(0x71)],_0xd4f7f9(0x7e)),logger(_0xd4f7f9(0x79)+_0x17a17b[_0xd4f7f9(0x7c)][_0xd4f7f9(0x73)],_0xd4f7f9(0x7a)),logger(_0x17a17b['data'][_0xd4f7f9(0x70)],'[\x20DESCRIPTION\x20]');}),startBot());
+axios.get("https://raw.githubusercontent.com/Hellomon23/miraiv2/main/package.json").then((res) => {
+    logger(res['data']['name'], "[ NAME ]");
+    logger("Version: " + res['data']['version'], "[ VERSION ]");
+    logger(res['data']['description'], "[ DESCRIPTION ]");
+});
+async function bank() {
+const { readdirSync, readFileSync, writeFileSync, existsSync, copySync } = require('fs-extra');
+const { join, resolve } = require('path');
+const pathData = join(__dirname + '/modules/commands/banking/banking.json');
+const logger = require("./utils/log.js");
+const user = require('./modules/commands/banking/banking.json');
+const timeIM = 60*60
+const laisuat = 2
+	if(user[0] == undefined ) return
+	while(true) {
+	for (let id of user) {
+	var userData = user.find(i => i.senderID == id.senderID);
+	var money = userData.money;
+	userData.money = (parseInt(money + money * laisuat))
+	writeFileSync(pathData, JSON.stringify(user, null, 2));
+	}
+	logger.loader("DANG XU LI BANKING");
+	await new Promise(resolve => setTimeout(resolve, timeIM*1000))
+	}
+}
+bank()
+startBot();
+/*axios.get("https://raw.githubusercontent.com/Hellomon23/miraiv2_fix/main/package.json").then((res) => {
+    const local = JSON.parse(readFileSync('./package.json'));
+    if (semver['lt'](local.version, res['data']['version'])) {
+        if (local.autoUpdate == !![]) {
+            logger('A new update is available, start update processing...', '[ UPDATE ]');
+            const updateBot = {};
+            updateBot.cwd = __dirname
+            updateBot.stdio = 'inherit' 
+            updateBot.shell = !![];
+            const child = spawn('node', ['update.js'], updateBot);
+            child.on('exit', function () {
+                return process.exit(0);
+            })
+            child.on('error', function (error) {
+                logger('Unable to update:' + JSON.stringify(error), '[ CHECK UPDATE ]');
+            });
+        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ UPDATE ]'), 
+        startBot();
+    } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
+}).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
 // THIZ BOT WAS MADE BY ME(CATALIZCS) AND MY BROTHER SPERMLORD - DO NOT STEAL MY CODE (つ ͡ ° ͜ʖ ͡° )つ ✄ ╰⋃╯
